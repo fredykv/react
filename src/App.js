851098e16c74863292865879b39ab3e5f5edd91d
Fredy <fredykv@hotmail.com>
2020-04-18 16:58:32 -0300

    primera vez

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    task:'',
+    tasks:[]
+  }
+  onChange=(e) => {
+    this.setState({task: e.target.value})
+    console.log(e.target.value);        
+  }
+  render() {
+    return (
+      <div className="container ">
+        <div className="row pt-5 ">
+          <div className="col-4"></div>
+          <div className="col-4 text-center border border-primary ">
+            <h1 className="mt-4">Bienvenido</h1>
+            <h4 className="mb-2">ingresa tus tareas</h4>
+            <form onSubmit="">
+              <input
+                type="text"
+                placeholder="escriba una tarea"
+                className="px-3 mb-2 py-1 text-center"
+                onChange={this.onChange}
+              />
+            </form>
+          </div>
+          <div className="col-4"></div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
