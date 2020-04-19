240dad9c3e80ec402d359172b767b354984c7f54
Fredy <fredykv@hotmail.com>
2020-04-19 18:41:49 -0300

    funcion dark modo

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ class App extends Component {
   state = {
     task: "",
     tasks: [],
-    mode: { mod: "Dark", clasName: "btn btn-dark" },
+    mode: {
+      mod: "Dark",
+      clasName: "btn btn-dark",
+      bg: "bg-dark text-white",
+      bgInput: "bg-light",
+    },
   };
   changeMode = (mode) => {
     if (mode === "Dark") {
-      const light = { mod: "Light", clasName: "btn btn-light" };
+      const light = {
+        mod: "Light",
+        clasName: "btn btn-light",
+        bg: "bg-light",
+        bgInput: "bg-dark text-white",
+      };
       this.setState({ mode: light });
-      
     } else {
-      const dark = { mod: "Dark", clasName: "btn btn-dark" };
+      const dark = {
+        mod: "Dark",
+        clasName: "btn btn-dark",
+        bg: "bg-dark text-white",
+        bgInput: "bg-light",
+      };
       this.setState({ mode: dark });
     }
   };
@@ -33,30 +47,41 @@ class App extends Component {
   };
   render() {
     const inputFijos = this.state.tasks.map((n, i) => (
-      <Input titulo={n.titulo} key={i} id={i} delete={this.delete} />
+      <Input
+        titulo={n.titulo}
+        key={i}
+        id={i}
+        delete={this.delete}
+        clase={this.state.mode.bgInput}
+      />
     ));
+    const clase =
+      "col-4 text-center pb-5 border border-primary " + this.state.mode.bg;
+    const claseInput = "px-3 mb-2 py-1 text-center " + this.state.mode.bgInput;
 
     return (
       <div className="container ">
         <div className="row pt-5 ">
-          <div className="col-4">
-            <Mode mode={this.state.mode} changeMode={this.changeMode} />
+          <div className="col-4 text-center ">
+            <div className="m-5 p-5">
+              <Mode mode={this.state.mode} changeMode={this.changeMode} />
+            </div>
           </div>
-          <div className="col-4 text-center border border-primary ">
+          <div className={clase}>
             <h1 className="mt-4">Bienvenido</h1>
             <h4 className="mb-2">ingresa tus tareas</h4>
             <form onSubmit={this.onSubmit}>
               <input
                 type="text"
                 placeholder="escriba una tarea"
-                className="px-3 mb-2 py-1 text-center"
+                className={claseInput}
                 value={this.state.task}
                 onChange={this.onChange}
               />
             </form>
             {inputFijos}
           </div>
-          <div className="col-4"></div>
+          <div className="col-4 "></div>
         </div>
       </div>
     );
--- src/componentes/Inputs.jsx
+++ src/componentes/Inputs.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Inputs = (props) => {
-    return (
-        <div>
-           <input type="text" className="my-2 text-center" placeholder={props.titulo}/> 
-           <button onClick={ () => props.delete(props.id)}>x</button>
-        </div>
-    );
+  const claseInputs = "my-2 text-center py-1 " + props.clase;
+  return (
+    <div >
+      <input type="text" className={claseInputs} value={props.titulo} />
+      <button
+        className={claseInputs}
+        style={{
+          fontSize: "18px",
+          marginLeft: "2px",
+          background: "red",
+          color: "black",
+          border: "none",
+          padding: "5px 12px",
+          borderRadius: "50%",
+          cursor: "pointer",
+        }}
+        onClick={() => props.delete(props.id)}
+      >
+        x
+      </button>
+    </div>
+  );
 };
 
 export default Inputs;
